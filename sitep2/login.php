4a3a2d8fc82052aa99dce5e2a837d00ffd4fe2f1
svdkolff <50834540+svan-der@users.noreply.github.com>
2019-10-03 13:19:27 +0200

    Add files via upload

---
+++ sitep2/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en" class="body">
+<head>
+    <meta charset="UTF-8">
+    <title>Login page</title>
+    <link rel="stylesheet" href="CSS/style.css" type="text/css">
+    <link href="https://fonts.googleapis.com/css?family=Big+Shoulders+Display|PT+Serif&display=swap" rel="stylesheet">
+</head>
+<body>
+ <div class="contact-form">
+    <br  /><br /><br  /><h2 value="LOGIN"">Welcome Admin!</h2>
+    <a href="index.html">Logout></a>
+   </div>
+   </body>
+</html>
+
